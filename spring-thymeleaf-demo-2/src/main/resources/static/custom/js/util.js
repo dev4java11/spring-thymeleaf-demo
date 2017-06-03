b3ddf499354091f400e2c9d77aaee1bb1eef92b7
hernxntk <hernxntk@gmail.com>
2017-06-03 14:39:06 -0500

    COMMIT

--- spring-thymeleaf-demo-2/src/main/resources/static/custom/js/util.js
+++ spring-thymeleaf-demo-2/src/main/resources/static/custom/js/util.js
@@ -1,9 +1,21 @@
 /**
  * 
  */
-(function(){
-	$.getUrlParameter = function (name){
-		var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
-		return results[1] || 0;
+
+function printMessage(messageIcon, messageHeader, messageBody, messageSeverity){
+	if(!messageSeverity){
+		messageSeverity = 'info';
 	}
-})();
+	if(!messageIcon){
+		messageIcon = 'fa fa-info';
+	}
+	
+	var growl = $.notify({
+		icon: messageIcon,
+		title: '<strong>'+messageHeader+': </strong>',
+		message: messageBody
+	},{
+		type: messageSeverity
+	});
+	return growl;
+}

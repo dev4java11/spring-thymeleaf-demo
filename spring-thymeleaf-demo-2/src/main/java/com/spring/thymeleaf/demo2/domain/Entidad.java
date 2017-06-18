a9049aad05891ab8ef8223264d9e81667d00f078
Hernan <Hernan@PC-Hernan91>
2017-06-18 11:04:48 -0500

    CAMBIOS NUEVOS ANTES QUERYDSL

--- spring-thymeleaf-demo-2/src/main/java/com/spring/thymeleaf/demo2/domain/Entidad.java
+++ spring-thymeleaf-demo-2/src/main/java/com/spring/thymeleaf/demo2/domain/Entidad.java
@@ -1,5 +1,23 @@
 package com.spring.thymeleaf.demo2.domain;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+
 public class Entidad {
 
+	public Pageable pageable;
+	
+	public void buildPageable(int page, int pageSize){
+		buildPageable(page, pageSize, null);
+	}
+	
+	public void buildPageable(int page, int pageSize, Sort sort){
+		pageable = new PageRequest(page, pageSize, sort);
+	}
+	
+	public void buildPageable(int page, int pageSize, Direction direction, String ... properties){
+		pageable = new PageRequest(page, pageSize, direction, properties);
+	}
 }
--- spring-thymeleaf-demo-2/src/main/java/com/spring/thymeleaf/demo2/domain/UsuarioRol.java
+++ spring-thymeleaf-demo-2/src/main/java/com/spring/thymeleaf/demo2/domain/UsuarioRol.java
@@ -6,6 +6,7 @@ import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -17,8 +18,8 @@ public class UsuarioRol extends Entidad {
 	private UsuarioRolPK id;
 	private Date fechaAsignacion;
 	
-	private Usuario usuario;
-	private Rol rol;
+//	private Usuario usuarioEntidad;
+//	private Rol rolEntidad;
 	
 	public UsuarioRol() {
 		id = new UsuarioRolPK();
@@ -41,23 +42,23 @@ public class UsuarioRol extends Entidad {
 		this.fechaAsignacion = fechaAsignacion;
 	}
 	
-	@OneToOne
-	@JoinColumn(name = "usuario", referencedColumnName = "usuario", nullable = true, insertable = false, updatable = false)
-	public Usuario getUsuario() {
-		return usuario;
-	}
+//	@ManyToOne
+//	@JoinColumn(name = "usuario", referencedColumnName = "usuario", nullable = false, insertable = false, updatable = false)
+//	public Usuario getUsuarioEntidad() {
+//		return usuarioEntidad;
+//	}
+//	
+//	public void setUsuarioEntidad(Usuario usuarioEntidad) {
+//		this.usuarioEntidad = usuarioEntidad;
+//	}
 	
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
-	}
-	
-	@OneToOne
-	@JoinColumn(name = "rol", referencedColumnName = "rol", nullable = true, insertable = false, updatable = false)
-	public Rol getRol() {
-		return rol;
-	}
-	
-	public void setRol(Rol rol) {
-		this.rol = rol;
-	}
+//	@OneToOne
+//	@JoinColumn(name = "rol", referencedColumnName = "rol", nullable = false, insertable = false, updatable = false)
+//	public Rol getRol() {
+//		return rol;
+//	}
+//	
+//	public void setRol(Rol rol) {
+//		this.rol = rol;
+//	}
 }
